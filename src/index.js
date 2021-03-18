3c1509a50f26ee6f5fce64d4700ff39f5a443b00
alvinmr <maulanaalvin1@gmail.com>
2021-03-18 10:48:45 +0800

    ganti gapake nochache

--- src/index.js
+++ src/index.js
@@ -5,14 +5,12 @@ const {
 const options = require('../utils/option')
 const color = require('../utils/index')
 const fs = require('fs')
-
-require('./msgHandler')
-noCache('./msgHandler', module => console.log(`'${module} updated!'`))
+const msgHandler = require('./msgHandler')
+// noCache('./msgHandler', module => console.log(`'${module} updated!'`))
 
 const start = (client = new Client()) => {
     console.log('[BOT] SERVER STARTED');
 
-    client.setPresence(true);
     client.onStateChanged((state) => {
         console.log('CLIENT STATE', state);
         if (state === "CONFLICT" || state === "UNLAUNCHED") return client.forceRefocus();
@@ -27,13 +25,13 @@ const start = (client = new Client()) => {
                     client.cutMsgCache()
                 }
             })
-        require('./msgHandler')(client, message)
+        msgHandler(client, message)
     })
 
     client.onAddedToGroup(async (chat) => {
         // await client.sendText(chat.groupMetadata.id, 'maap untuk sekarang gabisa dimasukin ke grup karna sudah banyak, coba hubungi pembuat bot ini biar bisa ditambahin heuheu ( ⇀‸↼‶ )').then(async () => await client.leaveGroup(chat.groupMetadata.id))
-        // let totMem = await chat.groupMetadata.participants.length
-        // if (totMem >= 1) client.sendText(chat.groupMetadata.id, `Halo rakyat grup ${chat.contact.name} terimakasih sudah menginvite bot ini, untuk melihat menu silahkan kirim *#menu*`)
+        let totMem = await chat.groupMetadata.participants.length
+        if (totMem >= 1) client.sendText(chat.groupMetadata.id, `Halo rakyat grup ${chat.contact.name} terimakasih sudah menginvite bot ini, untuk melihat menu silahkan kirim *#menu*`)
     })
 
     client.onIncomingCall((async (call) => {
